5dce392b98c0a6c3e3d435229b77546ec5da2935
fuadmostfij6 <fuadmostfij4@gmail.com>
2022-11-20 15:46:22 +0600

    fuad

--- lib/src/voice_message.dart
+++ lib/src/voice_message.dart
@@ -69,15 +69,8 @@ class _VoiceMessageState extends State<VoiceMessage>
       padding: EdgeInsets.symmetric(horizontal: .8.w()),
       constraints: BoxConstraints(maxWidth: 100.w() * .7),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(6.w()),
-          bottomLeft:
-              widget.me ? Radius.circular(6.w()) : Radius.circular(2.w()),
-          bottomRight:
-              !widget.me ? Radius.circular(6.w()) : Radius.circular(1.2.w()),
-          topRight: Radius.circular(6.w()),
-        ),
-        color: widget.me ? widget.meBgColor : widget.contactBgColor,
+        borderRadius: BorderRadius.circular(5),
+        color:  widget.meBgColor,
       ),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 4.w(), vertical: 2.8.w()),
